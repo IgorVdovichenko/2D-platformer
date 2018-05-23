01296f875eabe471b09f3b7434498dba3a213c88
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-23 11:49:24 +0200

    Add files via upload

---
+++ ButtonSound.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] AudioClip sound;
+
+    AudioSource audioSource;
+
+    private void Awake()
+	{
+		SetAudioSource();
+	}
+
+	private void SetAudioSource()
+	{
+		try
+		{
+			audioSource = GetAudioSource();
+		}
+		catch (System.Exception ex)
+		{
+			Debug.LogError(ex.Message);
+			enabled = false;
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+    {
+		audioSource.PlayOneShot(sound);
+    }
+
+	private AudioSource GetAudioSource()
+	{
+		AudioSource output;
+		output = GetComponent<AudioSource>();
+		if (output == null)
+		{
+			string message =
+				string.Format("Game object {0} misses required Audio Source component", gameObject.name);
+			throw new System.Exception(message);
+		}
+		else
+			return output;
+	}
+	
+}
