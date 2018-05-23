48000cc392a910004647b7d5eb55aeef6757ee52
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-23 15:08:23 +0200

    Add files via upload

---
+++ StageSelection/Stage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName ="New Stage", menuName ="Stage")]
+public class Stage : ScriptableObject
+{
+    [SerializeField] string sceneName;
+    [SerializeField] int bonusPoints;
+    [SerializeField] AudioClip music;
+
+    public bool cleared;
+
+    public string SceneName { get { return sceneName; } }
+    public int BonusPoints { get { return bonusPoints; } }
+    public AudioClip Music { get { return music; } }
+
+    public void LoadStage()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
