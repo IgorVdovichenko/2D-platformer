8ba32ce6084e58618929cd2e9b71af5ccfaba5ed
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-23 10:14:20 +0200

    Add files via upload

---
+++ Editor/EnemySpritesImporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnemySpritesImporter : AssetPostprocessor {
+
+    void OnPreprocessTexture()
+    {
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool isInDirectory = lowerCaseAssetPath.IndexOf("sprites/enemies/") != -1;
+
+        if (isInDirectory)
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+
+            textureImporter.spriteImportMode = SpriteImportMode.Multiple;
+
+            textureImporter.filterMode = FilterMode.Point;
+
+            textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+        }
+    }
+}
