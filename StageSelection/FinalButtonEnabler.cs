19069cd1fddee7ada11ac8f1ea59069958a872b8
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-23 22:20:37 +0200

    Add files via upload

---
+++ StageSelection/FinalButtonEnabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FinalButtonEnabler: IActivator
+{
+    public GameObject FinalButton { get; set; }
+    public GameObject CentralText { get; set; }
+
+    public FinalButtonEnabler(GameObject finalButton, GameObject centralText)
+    {
+        FinalButton = finalButton;
+        CentralText = centralText;
+    }
+
+    public void Activate()
+    {
+        FinalButton.SetActive(true);
+        CentralText.SetActive(false);
+    }
+
+	public void Deactivate()
+	{
+		FinalButton.SetActive(false);
+		CentralText.SetActive(true);
+	}
+}
