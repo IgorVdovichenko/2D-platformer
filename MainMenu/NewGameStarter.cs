2cc6c10f6236ea7ddafe7da10546845308f2ad8a
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-23 19:27:07 +0200

    Update NewGameStarter.cs

--- MainMenu/NewGameStarter.cs
+++ MainMenu/NewGameStarter.cs
@@ -6,32 +6,47 @@ class NewGameStarter : MonoBehaviour
 {
     [SerializeField] AudioClip StartGameSound;
     [SerializeField] float delayBeforeStart;
-    private readonly float minDelay = 1f;
+	private readonly float minDelay = 1f;
 
     [SerializeField] string stageToLoad = "Selection";
 
     AudioSource audioSource;
 
-    private float DelayBeforeStart
+    public float DelayBeforeStart
     {
         get
         {
-            if (delayBeforeStart < minDelay) delayBeforeStart = minDelay;
+            if (delayBeforeStart < minDelay)
+				delayBeforeStart = minDelay;
             return delayBeforeStart;
         }
         set
         {
-            if (value < minDelay) delayBeforeStart = minDelay;
+            if (value < minDelay)
+				delayBeforeStart = minDelay;
             delayBeforeStart = value;
         }
     }
 
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
+        SetAudioSource();
     }
 
-    //Attach this method in Unity to a button
+	private void SetAudioSource()
+	{
+		try
+		{
+			audioSource = Utilities.GetComponent<AudioSource>(gameObject);
+		}
+
+		catch (System.Exception ex)
+		{
+			Debug.LogError(ex.Message);
+			Utilities.StopEditorApplication();
+		}
+	}
+
     public void StartNewGame()
     {
         audioSource.PlayOneShot(StartGameSound);
@@ -44,16 +59,20 @@ class NewGameStarter : MonoBehaviour
         Button[] allButtons = FindObjectsOfType<Button>();
 
         foreach (var button in allButtons)
-        {
-            button.interactable = false;
+		{
+			button.interactable = false;
+			DisableButtonSounds(button);
+		}
+	}
 
-            ButtonSound buttonSound = button.gameObject.GetComponent<ButtonSound>();
-			if (buttonSound != null)
-				buttonSound.enabled = false;
-        }
-    }
+	private void DisableButtonSounds(Button button)
+	{
+		ButtonSound buttonSound = button.gameObject.GetComponent<ButtonSound>();
+		if (buttonSound != null)
+			buttonSound.enabled = false;
+	}
 
-    void Load()
+	void Load()
     {
         SceneManager.LoadScene(stageToLoad);
     }
