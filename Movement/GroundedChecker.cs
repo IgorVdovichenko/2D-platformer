40bf75fd403d934baa5296840347ff76018a5ec7
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-24 12:25:44 +0200

    Add files via upload

---
+++ Movement/GroundedChecker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GroundedChecker : ICheckIfGrounded
+{
+    GameObject objectToCheck;
+    string groundLayerMaskName;
+    float distanceToGround = 0.01f;
+
+    public GroundedChecker(GameObject obj, string groundLayerMaskName)
+    {
+        objectToCheck = obj;
+        this.groundLayerMaskName = groundLayerMaskName;
+    }
+
+    public bool IsGrounded()
+    {
+        LayerMask layerMask = LayerMask.GetMask(groundLayerMaskName);
+        RaycastHit2D hit =
+            Physics2D.Raycast(objectToCheck.transform.position, Vector2.down, distanceToGround, layerMask);
+
+        if (hit.collider != null)
+            return true;
+        else
+            return false;
+
+    }
+}
---
+++ Movement/ICheckIfGrounded.cs
@@ -0,0 +1,4 @@
+﻿public interface ICheckIfGrounded
+{
+    bool IsGrounded();
+}
