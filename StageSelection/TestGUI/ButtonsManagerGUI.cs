559c8a522f928bf52a850933a6636fdcbc53c23c
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-24 09:50:31 +0200

    Add files via upload

---
+++ StageSelection/TestGUI/ButtonsManagerGUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ButtonsManagerGUI : MonoBehaviour
+{
+    byte windowID = 10;
+    string windowLabel = "Stages Manager";
+    Vector2 topLeftWindowPos = new Vector2(50, 250);
+    Vector2 windowSize = new Vector2(150, 75);
+    Rect windowRect;
+
+    [SerializeField]
+    Stage[] stages;
+    IStageClearedStatusSetter setter;
+
+    private void Start()
+    {
+        windowRect = new Rect(topLeftWindowPos, windowSize);
+        setter = new StageClearedStatusSetter(stages);
+    }
+
+    private void OnGUI()
+    {
+
+        windowRect = GUILayout.Window(windowID, windowRect, setter.WindowFunction, windowLabel);
+
+    }
+}
---
+++ StageSelection/TestGUI/StageClearedStatusSetter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class StageClearedStatusSetter : IStageClearedStatusSetter
+{
+    Stage[] stages;
+
+    public StageClearedStatusSetter(Stage[] stages)
+    {
+        this.stages = stages;
+    }
+
+    public void WindowFunction(int windowID)
+    {
+        ShowToggles();
+        ShowButtons();
+        GUI.DragWindow();
+    }
+
+    void ShowToggles()
+    {
+        foreach (var stage in stages)
+        {
+            stage.cleared = GUILayout.Toggle(stage.cleared, stage.SceneName);
+        }
+    }
+
+    private void ShowButtons()
+    {
+        Button("Select All", SelectAll);
+        Button("Disselect All", DisselectAll);
+        Button("Update", () => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+    }
+
+    private void Button(string label, Action action)
+    {
+        if (GUILayout.Button(label))
+            action();
+    }
+
+    void SelectAll()
+    {
+        foreach (var stage in stages)
+        {
+            stage.cleared = true;
+        }
+    }
+
+    void DisselectAll()
+    {
+        foreach (var stage in stages)
+        {
+            stage.cleared = false;
+        }
+    }
+}
