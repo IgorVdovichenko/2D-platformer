062b5db8564e9b95882723c78c124bc8f830b69d
IgorVdovichenko <39549174+IgorVdovichenko@users.noreply.github.com>
2018-05-24 12:29:10 +0200

    Add files via upload

---
+++ Movement/LandingSoundController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class LandingSoundController : MonoBehaviour
+{
+    bool playedLandingSound = true;
+    public AudioClip groundedSound;
+    AudioSource sound;
+
+    public string groundLayerMaskName = "Floor";
+    GroundedChecker groundedChecker;
+
+    private void Awake()
+    {
+        try
+        {
+            sound = Utilities.GetComponent<AudioSource>(gameObject);
+            groundedChecker = new GroundedChecker(gameObject, groundLayerMaskName);
+        }
+        catch (Exception e)
+        {
+            Utilities.HandleException(e);
+        }
+    }
+
+    private void Update()
+    {
+        if (groundedChecker.IsGrounded() == false)
+            playedLandingSound = false;
+
+        PlayLandingSound();
+    }
+
+    private void PlayLandingSound()
+    {
+        if (groundedChecker.IsGrounded() && playedLandingSound == false)
+        {
+            sound.PlayOneShot(groundedSound);
+            playedLandingSound = true;
+        }
+    }
+}
